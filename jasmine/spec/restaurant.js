f11de4b36f6ef1280b29f75e6484bfafbd6dd3e3
Aron Roberts <robotros1@msn.com>
2018-08-09 20:41:00 -0700

    feat: add jasmine

---
+++ jasmine/spec/restaurant.js
@@ -0,0 +1,13 @@
+/* restaurant.js
+ *
+ * This is the spec file that Jasmine will read and contains
+ * all of the tests that will be run against your application.
+ */
+
+/* We're placing all of our tests within the $() function,
+ * since some of these tests may require DOM elements. We want
+ * to ensure they don't run until the DOM is ready.
+ */
+$(function() {
+   
+});
