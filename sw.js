037688da55da3461ebb0b77b0638ea19ce81123c
Aron Roberts <robotros1@msn.com>
2018-09-18 21:44:56 -0700

    updated cache method

--- sw.js
+++ sw.js
@@ -1,28 +1,38 @@
 "use strict";
 const cacheID = "mws-restaruant-001";
-
+const files = [
+  "/",
+  "/index.html",
+  "/restaurant.html",
+  "/css/styles.css",
+  "/js/dbhelper.js",
+  "/js/main.js",
+  "/js/restaurant_info.js",
+  "/js/register_sw.js"
+  "/data/restaurants.json"
+  "/img/1.jpg",
+  "/img/2.jpg",
+  "/img/3.jpg",
+  "/img/4.jpg",
+  "/img/5.jpg",
+  "/img/6.jpg",
+  "/img/7.jpg",
+  "/img/8.jpg",
+  "/img/9.jpg",
+  "/img/10.jpg",
+  ]
 
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(cacheID).then(cache => {
-    return cache.addAll(
-      [
-        "/",
-        "/index.html",
-        "/restaurant.html",
-        "/css/styles.css",
-        "/js/dbhelper.js",
-        "/js/main.js",
-        "/js/restaurant_info.js",
-        "/js/register_sw.js"
-      ]);
+    return cache.addAll(files);
     })
   );
 });
 
 self.addEventListener('fetch', function(event) {
   event.respondWith(
-    fetch(event.request).catch(function() {
-      return caches.match(event.request);
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
     })
   );
 });
