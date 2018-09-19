b8a24018c77f8b85f9ec9bae0033f5523984900c
Aron Roberts <robotros1@msn.com>
2018-09-18 23:38:52 -0700

    styling

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -23,7 +23,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1Ijoicm9ib3Ryb3MiLCJhIjoiY2prbmZlMHkxMHg0MjN4dGhjcTYyandjMCJ9.apvqNMFJVQu6xrhOp9S31g',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
