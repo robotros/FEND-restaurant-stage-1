46ff57fe8112285b154ecc6c2a4d6f45bcb52136
Aron Roberts <robotros1@msn.com>
2018-09-18 21:03:01 -0700

    feat: sw

--- sw.js
+++ sw.js
@@ -3,49 +3,26 @@ const cacheID = "mws-restaruant-001";
 
 
 self.addEventListener("install", event => {
-
   event.waitUntil(caches.open(cacheID).then(cache => {
-
-    return cache
-
-      .addAll([
-      "/",
-      "/index.html",
-      "/restaurant.html",
-      "/css/styles.css",
-      "/js/dbhelper.js",
-      "/js/main.js",
-      "/js/restaurant_info.js",
-      "/js/register.js"
-    ])
-
-      .catch((error) => {
-
-        console.log("Caches open failed: " + error);
-
-      });
-
-  }));
-
+    return cache.addAll(
+      [
+        "/",
+        "/index.html",
+        "/restaurant.html",
+        "/css/styles.css",
+        "/js/dbhelper.js",
+        "/js/main.js",
+        "/js/restaurant_info.js",
+        "/js/register_sw.js"
+      ]);
+    })
+  );
 });
 
-
-self.addEventListener("fetch", event => {
-  let cacheRequest = event.request;
-  let cacheUrlObj = new URL(event.request.url);
-  if (event.request.url.indexOf("restaurant.html") > -1) {
-    const cacheURL = "restaurant.html";
-    cacheRequest = new Request(cacheURL);
-  }
-   event.respondWith(
-       caches.match(cacheRequest).then((response) => {
-            return {
-               reponse || fetch(event.request).then((fetchResponse) => {
-                   return caches.open(cacheID).then(cache => {
-                       cache.put(event.request, fetchResponse.clone());
-                       return fetchResponse;
-                   })
-               })
-           }
-       })
-  }
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match(event.request);
+    })
+  );
+});
